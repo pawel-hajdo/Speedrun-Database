968a5931f0528c5540b130617471d5be416078f4
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-12-26 21:06:38 +0100

    created project structure, iniliazited springboot application

---
+++ server/src/main/java/com/speedrundatabaseapi/SpeedrunDatabaseApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.speedrundatabaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedrunDatabaseApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedrunDatabaseApiServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/speedrundatabaseapi/SpeedrunDatabaseApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedrundatabaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedrunDatabaseApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
