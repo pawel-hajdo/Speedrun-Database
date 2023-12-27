8a353619d9c962fed2c3497f7350a661ea43bfd0
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-12-27 19:19:49 +0100

    created UserController, UserService and UserRepository classes added post and get endpoints to controller

---
+++ server/src/main/java/com/speedrundatabaseapi/user/UserController.java
@@ -0,0 +1,30 @@
+package com.speedrundatabaseapi.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class UserController {
+
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/users")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping(path = "/register")
+    public void registerNewUser(User user){
+        userService.registerNewUser(user);
+    }
+}
---
+++ server/src/main/java/com/speedrundatabaseapi/user/UserService.java
@@ -0,0 +1,24 @@
+package com.speedrundatabaseapi.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void registerNewUser(User user){
+        userRepository.save(user);
+    }
+}
