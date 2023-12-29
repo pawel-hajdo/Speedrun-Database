62849c09c0323f2814d5179dd384a53714ceb82b
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-12-29 22:42:15 +0100

    added changeRunDetails method and endpoint

--- server/src/main/java/com/speedrundatabaseapi/run/RunController.java
+++ server/src/main/java/com/speedrundatabaseapi/run/RunController.java
@@ -1,5 +1,6 @@
 package com.speedrundatabaseapi.run;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,25 @@ public class RunController {
             logger.error("Error occurred while adding a new run");
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while adding a new run");
+        }
+    }
+
+    @PutMapping(path = "/{runId}")
+    public ResponseEntity<String> changeRunDetails(
+            @PathVariable long runId,
+            @RequestBody Run updatedRunDetails
+    ){
+        try{
+            runService.changeRunDetails(runId, updatedRunDetails);
+            logger.info("Run details updated successfully");
+            return ResponseEntity.ok("Run details updated successfully");
+        }catch (EntityNotFoundException e){
+            logger.info(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }catch (Exception e){
+            logger.error("Error while updating run details");
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while updating run details");
         }
     }
 }
