54633d7039396e82a565622e7b1b45d55e817dcd
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-01-02 11:09:40 +0100

    added checks in followUser method

--- server/src/main/java/com/speedrundatabaseapi/follow/FollowController.java
+++ server/src/main/java/com/speedrundatabaseapi/follow/FollowController.java
@@ -1,7 +1,10 @@
 package com.speedrundatabaseapi.follow;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import java.util.List;
 public class FollowController {
 
     private final FollowService followService;
+    private final Logger logger = LoggerFactory.getLogger(FollowController.class);
     @Autowired
     public FollowController(FollowService followService) {
         this.followService = followService;
@@ -27,10 +31,19 @@ public class FollowController {
     public ResponseEntity<String> followUser(@RequestBody FollowRequest followRequest){
         try{
             followService.followUser(followRequest.getFollowerId(), followRequest.getFollowingId());
+            logger.info("User followed successfully");
             return ResponseEntity.ok("User followed successfully");
         }catch (EntityNotFoundException e){
+            logger.info(e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }catch (DataIntegrityViolationException e){
+            logger.info("Tried to follow user that is already followed by user");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Tried to follow user that is already followed by user");
+        }catch (IllegalArgumentException e){
+            logger.info(e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }catch (Exception e){
+            logger.error("Error occurred while adding a new user");
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while following a user");
         }
--- server/src/main/java/com/speedrundatabaseapi/follow/FollowService.java
+++ server/src/main/java/com/speedrundatabaseapi/follow/FollowService.java
@@ -27,6 +27,9 @@ public class FollowService {
         User user1 = userRepository.findById(followerId).orElseThrow(() -> new EntityNotFoundException("User not found"));
         User user2 = userRepository.findById(followingId).orElseThrow(() -> new EntityNotFoundException("User not found"));
 
+        if(user1.getUserId() == user2.getUserId()){
+            throw new IllegalArgumentException("You can`t follow yourself");
+        }
         FollowKey followKey = new FollowKey(followerId, followingId);
 
         followRepository.save(new Follow(followKey, user1, user2));
